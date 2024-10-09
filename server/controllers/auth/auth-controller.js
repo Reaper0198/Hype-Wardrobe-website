5c4c2b0557670da7c774c06300cc9ad1a134b0de
kmTyagi198 <k.m.tyagi198@gmail.com>
2024-10-10 03:19:37 +0530

    added signup/login

--- server/controllers/auth/auth-controller.js
+++ server/controllers/auth/auth-controller.js
@@ -1,16 +1,35 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../../models/User')
-
+//import dotenv from 'dotenv'
 
 // register
-const register = async(req, res) =>{
+const registerUser = async(req, res) =>{
 
     const {userName, email, password} = req.body;
 
     try{
 
+        const checkUser = await User.findOne({email});
+        if(checkUser){
+            return res.json({
+                success : false,
+                message : "User already exist with this email.Please use different email."
+
+            })
+        }
+
         const hashPassword = await bcrypt.hash(password, 12);
+        const newUser = new User({
+            userName,
+            email,
+            password : hashPassword
+        })
+        await newUser.save();
+        res.status(200).json({
+            success : true,
+            message : "New Account Created Successfully"
+        })
 
     }catch(e){
         console.log(e);
@@ -24,8 +43,42 @@ const register = async(req, res) =>{
 
 
 // login 
-const login = async(req, res) =>{
+const loginUser = async(req, res) =>{
+
+    const { email, password} = req.body;
+
     try{
+
+        const checkUser = await User.findOne({email});
+        if(!checkUser){
+            return res.json({
+                success : false,
+                message : "User doesn't exist.Please create a account first."
+            })
+        }
+
+        const checkPasswordMatch = await bcrypt.compare(password, checkUser.password);
+        if(!checkPasswordMatch){
+            return res.json({
+                success : false,
+                message : "Incorrect Password. Please try again."
+            })
+        }
+        const token = jwt.sign({
+            id : checkUser._id,
+            role : checkUser.role,
+            email : checkUser.email,
+        },process.env.JWT_SECRET, {expiresIn : '60m'})
+
+        res.cookie('token', token, {httpOnly : true, secure : false}).json({
+            success : true,
+            message : 'Logged In successfully',
+            user : {
+                email : checkUser.email,
+                role : checkUser.role,
+                id : checkUser._id
+            }
+        })
 
     }catch(e){
         console.log(e);
@@ -39,3 +92,11 @@ const login = async(req, res) =>{
 
 
 // logout
+
+
+
+// Auth Middleware
+
+
+
+module.exports =  { registerUser, loginUser };
--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require("mongoose");
 const cookieParser = require('cookie-parser');
 const cors = require("cors");
+const authRouter = require ('./routes/auth-routes')
+require('dotenv').config();
 
 mongoose.connect('mongodb+srv://kmtyagi198:kmtyagi198!@cluster0.bsx51.mongodb.net/'
 ).then(()=>console.log("MongoDB connected...")).catch(error=>console.log(error));
@@ -12,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
     cors({
-        origin : 'http://localhost:5173/',
+        origin : 'http://localhost:5173',
         methods : ['GET', 'POST', 'DELETE', 'PUT'],
         allowedHeaders : [
             "Content-Type",
@@ -27,5 +29,7 @@ app.use(
 
 app.use(cookieParser());
 app.use(express.json());
+
+app.use('/api/auth', authRouter)
 
 app.listen(PORT, ()=> console.log(`Server is running on port ${PORT}`))
